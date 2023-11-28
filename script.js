e5115671d92f8c90c0f81f009caa9f8937f8e9b0
Hlulani Chauke <122245777+HlulaniChauke@users.noreply.github.com>
2023-11-28 16:42:32 +0900

    Add the read toggle button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function Book(author, title, pages) {
   this.author = author;
   this.title = title;
   this.pages = pages;
-  this.read = true;
+  this.read = false;
 }
 // add books to the array
 function addBookToLibrary(author, title, pages) {
@@ -26,28 +26,49 @@ function displayEachBook(i){
         const author = document.createElement('td');
         const pages = document.createElement('td');
         const readStatus = document.createElement('td');
+        const read = document.createElement('button')
+        const removeStatus = document.createElement('td');
         const remove = document.createElement('button');
 
         title.textContent = library[i].title;
         author.textContent = library[i].author;
         pages.textContent = library[i].pages;
-        readStatus.textContent = "Read";
-        remove.textContent = "Remove";
 
+        if (library[i].read === true){
+        read.textContent = "Read";
+        }else {read.textContent = "Not Read"}
+        read.classList.add("readToggle");
+
+        remove.textContent = "Remove";
         remove.classList.add('remove-button');
 
         row.appendChild(title);
         row.appendChild(author);
         row.appendChild(pages);
+        readStatus.appendChild(read);
         row.appendChild(readStatus);
+        readStatus.appendChild(remove);
         row.appendChild(remove);
 
 
         tableBooks.appendChild(row);
 }
+
 for (let i = 0; i< library.length; i++){
     displayEachBook(i);
+    console.log(library[i].read);
 }
+// const readButton = document.getElementsByClassName("readToggle");
+
+// readButton.addEventListener('click', (e) =>{
+//     const target = e.target;
+//     console.log(target.textContent);
+//     if (target.textContent === "Read"){
+//         target.textContent = "Not Read";
+//     }else{
+//         target.textContent = "Read";
+//     }
+// });
 
 tableBooks.addEventListener('click', (e) => {
     const target = e.target;
@@ -55,6 +76,13 @@ tableBooks.addEventListener('click', (e) => {
         const row = target.parentNode;
         const table = row.parentNode;
         table.removeChild(row);
+    }
+
+    console.log(target.textContent);
+    if (target.textContent === "Read"){
+        target.textContent = "Not Read";
+    }else{
+        target.textContent = "Read";
     }
 });
 
